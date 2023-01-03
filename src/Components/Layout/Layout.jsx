9c3e9aa071e243037a38ae58bf9013a303022c1e
Alan Lai <alan.n.lai@gmail.com>
2023-01-02 23:15:00 -0800

    removed link ref

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-import { Link } from "react-router-dom";
 
 const Layout = ({ children }) => {
 
