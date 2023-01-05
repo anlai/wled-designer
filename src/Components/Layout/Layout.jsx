520e8933232622d6dc7f55091b4e2181f5762b2a
Alan Lai <alan.n.lai@gmail.com>
2023-01-04 23:24:14 -0800

    updated class to className

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -6,16 +6,16 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <header class="d-flex flex-wrap justify-content-center py-3 border-bottom">
+      <header className="d-flex flex-wrap justify-content-center py-3 border-bottom">
         <span className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <span class="fs-4">WLED Designer</span>
+          <span className="fs-4">WLED Designer</span>
         </span>
         <Navbar></Navbar>
       </header>
 
       <main><Outlet></Outlet></main>
 
-      <footer class="pt-5 my-5 text-muted border-top">
+      <footer className="pt-5 my-5 text-muted border-top">
           Created by the <a href="https://github.com/anlai">anlai</a> &middot; &copy; 2022
       </footer>
     </>
