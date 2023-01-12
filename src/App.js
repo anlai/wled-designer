74784facb03d1a81ffba61992695672dcf4e5d6c
Alan Lai <alan.n.lai@gmail.com>
2023-01-11 19:58:23 -0800

    fix for loading?

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
